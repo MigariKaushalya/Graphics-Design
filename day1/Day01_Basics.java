7e9a5a7c827e248024e175b6131c6217bee97c22
MigariKaushalya <migarikawshalya@gmail.com>
2026-08-14 22:01:47 +0530

    Add files via upload

---
+++ day1/Day01_Basics.java
@@ -0,0 +1,16 @@
+/*
+ DAY 01 - COMPUTER GRAPHICS BASICS
+ Learn: pixels, coordinates, RGB colors, raster vs vector graphics.
+ Goal: understand how an image is represented on a screen.
+*/
+public class Day01_Basics {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 80;
+        int red = 255, green = 100, blue = 50;
+
+        System.out.println("Point = (" + x + ", " + y + ")");
+        System.out.println("RGB = (" + red + ", " + green + ", " + blue + ")");
+        System.out.println("Practice: change x, y and RGB values.");
+    }
+}
